63479d2a60eedb202a12a1a8df0965802d5bfdbc
Sebastian Spagnolo <seba@Sebastians-MacBook-Pro.local>
2022-01-26 03:38:09 +0000

    passed 02 repeatStrings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (string, times) {
+    for (let i = 0; i <= 10; ++i) {
+        if (times < 0) { return ('ERROR') }
+        else { return (string.repeat(times)) }
+    }
 };
-
 // Do not edit below this line
 module.exports = repeatString;
